7baa130be01a3fe8dc52150321bfc99f83d376ac
Paul Teter <pteter20@stjohnschs.org>
2018-01-29 17:28:55 -0500

    Made winchUp work

--- src/main/java/org/usfirst/frc/team4456/commands/winchUp.java
+++ src/main/java/org/usfirst/frc/team4456/commands/winchUp.java
@@ -6,4 +6,17 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class winchUp extends Command{
 
+    boolean finished;
+
+    public winchUp() { requires(Robot.winch); }
+
+    protected void initialize() {
+        Robot.winch.winchUp();
+    }
+
+    protected boolean isFinished() { return finished; }
+
+    protected void end() {Robot.winch.winchStop();}
+
+    protected void interrupted() { end(); }
 }
