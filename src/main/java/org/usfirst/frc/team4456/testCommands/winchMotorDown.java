22a3f81e5c89f990bbabfb0b08485b314bf06280
Chris Martinez <camartinez0524@gmail.com>
2018-02-19 11:45:53 -0500

    Fixed winchMotorDown and winchMotorUp

--- src/main/java/org/usfirst/frc/team4456/testCommands/winchMotorDown.java
+++ src/main/java/org/usfirst/frc/team4456/testCommands/winchMotorDown.java
@@ -1,4 +1,22 @@
 package org.usfirst.frc.team4456.testCommands;
 
-public class winchMotorDown {
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team4456.Robot;
+import org.usfirst.frc.team4456.RobotMap;
+
+public class winchMotorDown extends Command {
+    public winchMotorDown() { requires(Robot.winch); }
+
+    protected void initialize() {
+        Robot.winch.winchDown();
+    }
+
+    protected boolean isFinished() { return true; }
+
+    protected void end() {Robot.winch.winchStop();}
+
+    protected void interrupted() { end(); }
+
+
 }
--- src/main/java/org/usfirst/frc/team4456/testCommands/winchMotorUp.java
+++ src/main/java/org/usfirst/frc/team4456/testCommands/winchMotorUp.java
@@ -1,4 +1,21 @@
 package org.usfirst.frc.team4456.testCommands;
 
-public class winchMotorUp {
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team4456.Robot;
+
+public class winchMotorUp extends Command {
+
+
+    public winchMotorUp() { requires(Robot.winch); }
+
+    protected void initialize() {
+        Robot.winch.winchUp();
+    }
+
+    protected boolean isFinished() { return true; }
+
+    protected void end() {Robot.winch.winchStop();}
+
+    protected void interrupted() { end(); }
+
 }
